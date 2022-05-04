ef41ae1fb03b1a27ff883c7f8622e6233c7e15b0
sadra <sadrataghavi1383@gmail.com>
2022-05-04 13:47:43 +0430

    linked dashboard header to main dhasboard

--- App/pages/Dashboard.tsx
+++ App/pages/Dashboard.tsx
@@ -2,15 +2,22 @@ import React from 'react'
 
 // before you code; lets discuss about the general layout and other stuff.
 
+// style
+import './style/dashboard.scss'
+
 // sections
 import DashboardSidebar from 'comps/DashbaordSidebar'
 import DashboardData from 'comps/DashboardData'
+import DashboardHeader from 'comps/DashboardHeader'
 
 const Dashboard = () => {
     return (
         <div className='dashboard-container'>
-            <DashboardSidebar />
-            <DashboardData />
+            <DashboardHeader />
+            <div className='dashboard-wrapper'>
+                <DashboardSidebar />
+                <DashboardData />
+            </div>
         </div>
     )
 }
