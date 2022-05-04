30185d17868b6eea16164be49dce25d21f46089b
sadra <sadrataghavi1383@gmail.com>
2022-05-04 13:47:22 +0430

    dashboard header file initated

---
+++ App/components/DashboardHeader.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// style
+import './style/header.scss'
+
+const DashboardHeader = () => {
+    return <div className='dashboard-header'>DashboardHeader</div>
+}
+
+export default DashboardHeader
