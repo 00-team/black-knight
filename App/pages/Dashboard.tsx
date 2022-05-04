862d540c1a7c34810e45a80024b654cbd016ef3b
sadra <sadrataghavi1383@gmail.com>
2022-05-04 13:25:54 +0430

    imported new sections to main dashboard

--- App/pages/Dashboard.tsx
+++ App/pages/Dashboard.tsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 // before you code; lets discuss about the general layout and other stuff.
 
+// sections
+import DashboardSidebar from 'comps/DashbaordSidebar'
+import DashboardData from 'comps/DashboardData'
+
 const Dashboard = () => {
-    return <div>Dashboard</div>
+    return (
+        <div className='dashboard-container'>
+            <DashboardSidebar />
+            <DashboardData />
+        </div>
+    )
 }
 
 export default Dashboard
