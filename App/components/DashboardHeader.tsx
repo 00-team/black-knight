96bd2f37ed86addc4e620d0a05118b13a27e78f0
sadra <sadrataghavi1383@gmail.com>
2022-05-04 14:25:07 +0430

    some major improvements on dashboard header section

--- App/components/DashboardHeader.tsx
+++ App/components/DashboardHeader.tsx
@@ -3,8 +3,29 @@ import React from 'react'
 // style
 import './style/header.scss'
 
+// icons
+import { FaSortDown } from '@react-icons/all-files/fa/FaSortDown'
+
+// imgs
+const default_img = require('../static/imgs/default_male.png')
+
 const DashboardHeader = () => {
-    return <div className='dashboard-header'>DashboardHeader</div>
+    return (
+        <div className='dashboard-header'>
+            <div className='active-section'>--ACTIVE SECTION--</div>
+            <div className='user-section'>
+                <div className='user-section-wrapper'>
+                    <div className='profile-img'>
+                        <img src={default_img} alt='' />
+                    </div>
+                    <div className='dropdown-icon'>
+                        <FaSortDown size={24} />
+                    </div>
+                </div>
+                <div className='dropdown'></div>
+            </div>
+        </div>
+    )
 }
 
 export default DashboardHeader
