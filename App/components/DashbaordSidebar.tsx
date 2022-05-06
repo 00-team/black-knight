ce35144a58d347b985d9c1fa6c4029d8a02b4a95
sadra <sadrataghavi1383@gmail.com>
2022-05-06 19:34:15 +0430

    sidebar wrapper improved

--- App/components/DashbaordSidebar.tsx
+++ App/components/DashbaordSidebar.tsx
@@ -6,8 +6,32 @@ import './style/sidebar.scss'
 const DashboardSidebar = () => {
     return (
         <div className='sidebar-container'>
-            <div className='sidebar-header title_small'>
+            <div className='sidebar-header title'>
                 <span>Dashboard</span>
+            </div>
+            <div className='sidebar-wrapper'>
+                <div className='sidebar-category-wrappper'>
+                    <div className='category title_small'>
+                        <span>Category 1</span>
+                    </div>
+                    <div className='column description'>
+                        <div className='icon'></div>
+                        <div className='holder'>Users</div>
+                    </div>
+                    <div className='column description'>
+                        <div className='icon'></div>
+                        <div className='holder'>Users</div>
+                    </div>
+                </div>
+                <div className='sidebar-category-wrappper'>
+                    <div className='category title_small'>
+                        <span>Category 2</span>
+                    </div>
+                    <div className='column description'>
+                        <div className='icon'></div>
+                        <div className='holder'>Users</div>
+                    </div>
+                </div>
             </div>
         </div>
     )
