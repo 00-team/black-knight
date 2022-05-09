49195b2045232b75994a03c3bf48905a8f987686
007 <dr007cc@gmail.com>
2022-05-09 17:47:47 +0430

    updating the default_avatar

--- black_knight/admin/sites.py
+++ black_knight/admin/sites.py
@@ -26,7 +26,7 @@ INVALID_LOGIN_DATA = E('Invalid Login Data!')
 
 class AdminSite(admin.AdminSite):
     template = 'black-knight.html'
-    default_avatar = settings.STATIC_URL + 'avatar.jpg'
+    default_avatar = settings.STATIC_URL + 'black_knight/default_avatar.jpg'
 
     def __init__(self, name='black_knight'):
         return super().__init__(name)
