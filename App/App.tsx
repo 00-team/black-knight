0d119378af7df76a1c5c5a215c247d918a9d4ed6
007 <dr007cc@gmail.com>
2022-05-09 23:58:41 +0430

    using the state in App

--- App/App.tsx
+++ App/App.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect } from 'react'
 
 // router
 import { Routes, Route } from 'react-router-dom'
@@ -9,13 +9,22 @@ import './style/base.scss'
 // components
 import Dashboard from './pages/Dashboard'
 
+// state
+import { MainAtom } from 'state'
+import { useAtom } from 'jotai'
+
 const App: FC = () => {
+    const [response] = useAtom(MainAtom)
+    useEffect(() => {
+        console.log(response)
+    }, [response])
+
     return (
         // /admin/*
         <Routes>
             <Route path='/' element={<Dashboard />} />
             {/* <Route path='/login/' element={<Login />} /> */}
-            {/* <Route path='/AppX/ModelX/IdX/' element={<Login />} /> */}
+            {/* <Route path='/AppX/ModelX/IdX/' element={<ModelX />} /> */}
         </Routes>
     )
 }
