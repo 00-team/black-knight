b10aea8dabf98b2531a796de0b071520acdd9992
007 <dr007cc@gmail.com>
2022-05-09 15:26:11 +0430

    moving static/black_knight_dist to static/black_knight/dist

--- webpack/config/django-html.ts
+++ webpack/config/django-html.ts
@@ -10,7 +10,7 @@ const HtmlPG = new HtmlWP({
     filename: resolve(black_knight, 'templates/black-knight.html'),
     template: resolve(APP_DIR, 'templates/django.html'),
     inject: true,
-    publicPath: '/black_knight_dist/',
+    publicPath: '/black_knight/dist/',
     minify: false,
 })
 
--- webpack/config/path.ts
+++ webpack/config/path.ts
@@ -3,6 +3,6 @@ import { resolve } from 'path'
 const BASE_DIR = resolve(__dirname, '../../')
 const APP_DIR = resolve(BASE_DIR, 'App')
 const black_knight = resolve(BASE_DIR, 'black_knight')
-const DIST_DIR = resolve(black_knight, 'static/black_knight_dist/')
+const DIST_DIR = resolve(black_knight, 'static/black_knight/dist/')
 
 export { BASE_DIR, APP_DIR, DIST_DIR, black_knight, resolve }
