7f77833339de6c5ce748e9ed1646e5e3d29e0443
sadra <sadrataghavi1383@gmail.com>
2022-05-18 10:28:16 +0430

    some logoutbutton eventlisteners added

--- App/components/utils/LogoutButton.tsx
+++ App/components/utils/LogoutButton.tsx
@@ -1,12 +1,114 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 // style
 import './style/logout.scss'
 
+const logoutButtonStates = {
+    default: {
+        '--figure-duration': '100',
+        '--transform-figure': 'none',
+        '--walking-duration': '100',
+        '--transform-arm1': 'none',
+        '--transform-wrist1': 'none',
+        '--transform-arm2': 'none',
+        '--transform-wrist2': 'none',
+        '--transform-leg1': 'none',
+        '--transform-calf1': 'none',
+        '--transform-leg2': 'none',
+        '--transform-calf2': 'none',
+    },
+    hover: {
+        '--figure-duration': '100',
+        '--transform-figure': 'translateX(1.5px)',
+        '--walking-duration': '100',
+        '--transform-arm1': 'rotate(-5deg)',
+        '--transform-wrist1': 'rotate(-15deg)',
+        '--transform-arm2': 'rotate(5deg)',
+        '--transform-wrist2': 'rotate(6deg)',
+        '--transform-leg1': 'rotate(-10deg)',
+        '--transform-calf1': 'rotate(5deg)',
+        '--transform-leg2': 'rotate(20deg)',
+        '--transform-calf2': 'rotate(-20deg)',
+    },
+    walking1: {
+        '--figure-duration': '300',
+        '--transform-figure': 'translateX(11px)',
+        '--walking-duration': '300',
+        '--transform-arm1': 'translateX(-4px) translateY(-2px) rotate(120deg)',
+        '--transform-wrist1': 'rotate(-5deg)',
+        '--transform-arm2': 'translateX(4px) rotate(-110deg)',
+        '--transform-wrist2': 'rotate(-5deg)',
+        '--transform-leg1': 'translateX(-3px) rotate(80deg)',
+        '--transform-calf1': 'rotate(-30deg)',
+        '--transform-leg2': 'translateX(4px) rotate(-60deg)',
+        '--transform-calf2': 'rotate(20deg)',
+    },
+    walking2: {
+        '--figure-duration': '400',
+        '--transform-figure': 'translateX(17px)',
+        '--walking-duration': '300',
+        '--transform-arm1': 'rotate(60deg)',
+        '--transform-wrist1': 'rotate(-15deg)',
+        '--transform-arm2': 'rotate(-45deg)',
+        '--transform-wrist2': 'rotate(6deg)',
+        '--transform-leg1': 'rotate(-5deg)',
+        '--transform-calf1': 'rotate(10deg)',
+        '--transform-leg2': 'rotate(10deg)',
+        '--transform-calf2': 'rotate(-20deg)',
+    },
+    falling1: {
+        '--figure-duration': '1600',
+        '--walking-duration': '400',
+        '--transform-arm1': 'rotate(-60deg)',
+        '--transform-wrist1': 'none',
+        '--transform-arm2': 'rotate(30deg)',
+        '--transform-wrist2': 'rotate(120deg)',
+        '--transform-leg1': 'rotate(-30deg)',
+        '--transform-calf1': 'rotate(-20deg)',
+        '--transform-leg2': 'rotate(20deg)',
+    },
+    falling2: {
+        '--walking-duration': '300',
+        '--transform-arm1': 'rotate(-100deg)',
+        '--transform-arm2': 'rotate(-60deg)',
+        '--transform-wrist2': 'rotate(60deg)',
+        '--transform-leg1': 'rotate(80deg)',
+        '--transform-calf1': 'rotate(20deg)',
+        '--transform-leg2': 'rotate(-60deg)',
+    },
+    falling3: {
+        '--walking-duration': '500',
+        '--transform-arm1': 'rotate(-30deg)',
+        '--transform-wrist1': 'rotate(40deg)',
+        '--transform-arm2': 'rotate(50deg)',
+        '--transform-wrist2': 'none',
+        '--transform-leg1': 'rotate(-30deg)',
+        '--transform-leg2': 'rotate(20deg)',
+        '--transform-calf2': 'none',
+    },
+}
+
 const LogoutButton = () => {
+    const [LogoutButtonStatus, setLogoutButtonStatus] = useState('default')
+
     return (
         <div className='logout-button-container'>
-            <button className='logoutButton'>
+            <button
+                className='logoutButton'
+                onMouseEnter={() => {
+                    if (LogoutButtonStatus === 'default')
+                        setLogoutButtonStatus('hover')
+                }}
+                onMouseLeave={() => {
+                    if (LogoutButtonStatus === 'hover')
+                        setLogoutButtonStatus('default')
+                }}
+                style={
+                    LogoutButtonStatus === 'hover'
+                        ? (logoutButtonStates.hover as React.CSSProperties)
+                        : (logoutButtonStates.default as React.CSSProperties)
+                }
+            >
                 <span className='button-text'>Log Out</span>
 
                 <svg className='doorway' viewBox='0 0 100 100'>
