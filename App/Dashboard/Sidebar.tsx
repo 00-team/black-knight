2cfc1a0ec45d51b15f9a23cb78d62bd7aa78ee35
sadra <sadrataghavi1383@gmail.com>
2022-05-31 19:34:23 +0430

    added 2 svgs to sidebar and linked

--- App/Dashboard/Sidebar.tsx
+++ App/Dashboard/Sidebar.tsx
@@ -21,10 +21,16 @@ const Sidebar: FC = () => {
 
                         {app.models.map((model, index) => (
                             <div className='column title_smaller' key={index}>
-                                <div className='icon'>
-                                    <img src={model.icon || default_icon} />
+                                <div className='right-side'>
+                                    <div className='icon'>
+                                        <img src={model.icon || default_icon} />
+                                    </div>
+                                    <div className='holder'>{model.name}</div>
                                 </div>
-                                <div className='holder'>{model.name}</div>
+                                <div className='left-side'>
+                                    <AddSvg />
+                                    <DeleteSvg />
+                                </div>
                             </div>
                         ))}
                     </div>
@@ -35,3 +41,39 @@ const Sidebar: FC = () => {
 }
 
 export default Sidebar
+
+const AddSvg = () => {
+    return (
+        <div className='add-container'>
+            <svg
+                xmlns='http://www.w3.org/2000/svg'
+                width='13'
+                height='13'
+                viewBox='0 0 1792 1792'
+            >
+                <path
+                    fill='#70bf2b'
+                    d='M1600 796v192q0 40-28 68t-68 28h-416v416q0 40-28 68t-68 28h-192q-40 0-68-28t-28-68v-416h-416q-40 0-68-28t-28-68v-192q0-40 28-68t68-28h416v-416q0-40 28-68t68-28h192q40 0 68 28t28 68v416h416q40 0 68 28t28 68z'
+                />
+            </svg>
+        </div>
+    )
+}
+
+const DeleteSvg = () => {
+    return (
+        <div className='delete-container'>
+            <svg
+                xmlns='http://www.w3.org/2000/svg'
+                width='13'
+                height='13'
+                viewBox='0 0 1792 1792'
+            >
+                <path
+                    fill='#efb80b'
+                    d='M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z'
+                />
+            </svg>
+        </div>
+    )
+}
