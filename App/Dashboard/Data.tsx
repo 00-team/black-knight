cae6726f8355b52b7f44450d4f0f2f733b6665b9
sadra <sadrataghavi1383@gmail.com>
2022-06-03 12:46:33 +0430

    some jsx conditions and working on data container default mode

--- App/Dashboard/Data.tsx
+++ App/Dashboard/Data.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { FC } from 'react'
 
 import './style/data.scss'
 
-const DashboardData = () => {
-    return <div className='dashboard-data'>data</div>
+interface DashboardDataProps {
+    SectionActive: boolean
+}
+
+const DashboardData: FC<DashboardDataProps> = ({ SectionActive }) => {
+    return (
+        <>
+            {!SectionActive && (
+                <div className='dashboard-data no-section title'>
+                    <span>P</span>
+                    <span>l</span>
+                    <span>e</span>
+                    <span>a</span>
+                    <span>s</span>
+                    <span>e</span>
+                    <span className='space'></span>
+                    <span>S</span>
+                    <span>e</span>
+                    <span>l</span>
+                    <span>e</span>
+                    <span>c</span>
+                    <span>t</span>
+                    <span className='space'></span>
+                    <span>A</span>
+                    <span className='space'></span>
+                    <span>M</span>
+                    <span>o</span>
+                    <span>d</span>
+                    <span>e</span>
+                    <span>l</span>
+                </div>
+            )}
+            {SectionActive && <div className='dashboard-data'>data</div>}
+        </>
+    )
 }
 
 export default DashboardData
