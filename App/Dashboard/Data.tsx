a01b6fc459bcef8a4c2481be30be27a5d5d13c66
sadra <sadrataghavi1383@gmail.com>
2022-06-03 12:55:00 +0430

    jsx for 'please select a model' added and spans generated

--- App/Dashboard/Data.tsx
+++ App/Dashboard/Data.tsx
@@ -11,27 +11,27 @@ const DashboardData: FC<DashboardDataProps> = ({ SectionActive }) => {
         <>
             {!SectionActive && (
                 <div className='dashboard-data no-section title'>
-                    <span>P</span>
-                    <span>l</span>
-                    <span>e</span>
-                    <span>a</span>
-                    <span>s</span>
-                    <span>e</span>
+                    <span style={{ animationDelay: '0' }}>P</span>
+                    <span style={{ animationDelay: '0.5s' }}>l</span>
+                    <span style={{ animationDelay: '1s' }}>e</span>
+                    <span style={{ animationDelay: '1.5s' }}>a</span>
+                    <span style={{ animationDelay: '2s' }}>s</span>
+                    <span style={{ animationDelay: '2.5s' }}>e</span>
                     <span className='space'></span>
-                    <span>S</span>
-                    <span>e</span>
-                    <span>l</span>
-                    <span>e</span>
-                    <span>c</span>
-                    <span>t</span>
+                    <span style={{ animationDelay: '3s' }}>S</span>
+                    <span style={{ animationDelay: '3.5s' }}>e</span>
+                    <span style={{ animationDelay: '4s' }}>l</span>
+                    <span style={{ animationDelay: '4.5s' }}>e</span>
+                    <span style={{ animationDelay: '5s' }}>c</span>
+                    <span style={{ animationDelay: '5.5s' }}>t</span>
                     <span className='space'></span>
-                    <span>A</span>
+                    <span style={{ animationDelay: '6s' }}>A</span>
                     <span className='space'></span>
-                    <span>M</span>
-                    <span>o</span>
-                    <span>d</span>
-                    <span>e</span>
-                    <span>l</span>
+                    <span style={{ animationDelay: '6.5s' }}>M</span>
+                    <span style={{ animationDelay: '7s' }}>o</span>
+                    <span style={{ animationDelay: '7.5s' }}>d</span>
+                    <span style={{ animationDelay: '8s' }}>e</span>
+                    <span style={{ animationDelay: '8.5s' }}>l</span>
                 </div>
             )}
             {SectionActive && <div className='dashboard-data'>data</div>}
