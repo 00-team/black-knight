e0259c765d9b8d413d2941332cf4013048308359
007 <dr007cc@gmail.com>
2022-06-14 10:07:26 +0430

    updating Loging stuff

--- App/Login/index.tsx
+++ App/Login/index.tsx
@@ -1,21 +1,80 @@
 import React, { FC, useEffect, useState } from 'react'
 
-import { useSearchParams } from 'react-router-dom'
+import { useSearchParams, useNavigate } from 'react-router-dom'
 
-interface LoginProps {}
+import { Login as AuthLogin } from 'state/api/auth'
 
-const Login: FC<LoginProps> = () => {
+interface TAuthData {
+    username: string
+    password: string
+}
+
+const Login: FC = () => {
+    const [AuthData, setAuthData] = useState<TAuthData>({
+        username: '',
+        password: '',
+    })
+
+    // for stroing ?next=/admin/12/
     const [NextURL, setNextURL] = useState<string | null>(null)
+
+    const navigate = useNavigate()
     const [SearchParams, setSearchParams] = useSearchParams()
 
     useEffect(() => {
-        setNextURL(SearchParams.get('next'))
-        setSearchParams('')
+        let n = SearchParams.get('next')
+        // by defualt next is /admin/ and we dont want it like this.
+        if (n !== BASE_URL) setNextURL(n)
+
+        // delete only next=... from query params
+        SearchParams.delete('next')
+        setSearchParams(SearchParams)
     }, [])
 
-    console.log(NextURL)
+    const Submit = async () => {
+        if (await AuthLogin(AuthData)) navigate(GetNextURL(NextURL))
+    }
 
-    return <div>Login</div>
+    return (
+        <div>
+            <div>
+                username:
+                <input
+                    type='text'
+                    autoComplete='on'
+                    name='username'
+                    onChange={e =>
+                        setAuthData({ ...AuthData, username: e.target.value })
+                    }
+                />
+            </div>
+            <div>
+                password:
+                <input
+                    type='text'
+                    autoComplete='on'
+                    name='password'
+                    onChange={e =>
+                        setAuthData({ ...AuthData, password: e.target.value })
+                    }
+                />
+            </div>
+            <div>
+                <button onClick={() => Submit()}>Submit</button>
+            </div>
+        </div>
+    )
+}
+
+const GetNextURL = (next: string | null) => {
+    if (!next) return '/'
+
+    if (next.at(-1) != '/') next = next + '/'
+    if (next.startsWith(BASE_URL)) next = next.slice(BASE_URL.length)
+
+    if (next.at(0) != '/') return '/' + next
+
+    return next
 }
 
 export default Login
--- App/state/api/auth.ts
+++ App/state/api/auth.ts
@@ -1,9 +1,19 @@
-import { GET } from '../utils'
+import { GET, POST } from '../utils'
 
 const Logout = async () => {
-    const response = await GET('/api/logout')
+    const response = await GET('/api/logout/')
     if (response.ok) location.assign('/')
-    else alert(response.error.message)
+    else alert(response.message)
 }
 
-export { Logout }
+interface LoginData {
+    username: string
+    password: string
+}
+
+const Login = async (data: LoginData) => {
+    const response = await POST('/api/login/', data)
+    return response.ok
+}
+
+export { Logout, Login }
