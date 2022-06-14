ff519d6b8ecf6efab3a9a69b2c934cf78bc8011e
007 <dr007cc@gmail.com>
2022-06-14 09:25:41 +0430

    adding POST request in state request utils

--- App/state/utils/request.ts
+++ App/state/utils/request.ts
@@ -1,52 +1,83 @@
 import axios, { AxiosRequestConfig } from 'axios'
+import { get as GetCookies } from 'js-cookie'
 
 const U = (url: string) => (BASE_URL + url).replaceAll('//', '/')
 
-interface ResError {
-    message: string
-    code: number
-}
-
-interface GetResponseOk {
+interface ResponseOk {
     ok: true
     data: any
 }
 
-interface GetResponseError {
+interface ResponseError {
     ok: false
-    error: ResError
+    message: string
+    code: number
 }
 
-type GetResponse = GetResponseOk | GetResponseError
+type Response = ResponseOk | ResponseError
 
-const HandleError = (error: unknown): ResError => {
+const HandleError = (error: unknown): ResponseError => {
     if (axios.isAxiosError(error)) {
         if (error.response) {
             if (error.response.data.message) return error.response.data
 
             return {
+                ok: false,
                 message: error.response.statusText,
                 code: error.response.status,
             }
         }
 
         return {
+            ok: false,
             message: error.message,
             code: 520,
         }
     }
 
-    return { message: 'An Unknown Error Happend!', code: 520 }
+    return { ok: false, message: 'An Unknown Error Happend!', code: 520 }
 }
 
-type TGET = (url: string, config?: AxiosRequestConfig) => Promise<GetResponse>
+const GetCSRFToken = (config?: AxiosRequestConfig): AxiosRequestConfig => {
+    const csrftoken = GetCookies('csrftoken')
+    if (!csrftoken) {
+        location.reload()
+        throw Error('CSRF Token Required.')
+    }
+    const BaseHeaders = { 'X-CSRFToken': csrftoken }
+
+    if (!config) return { headers: BaseHeaders }
+    if (!config.headers) return { ...config, headers: BaseHeaders }
+    if (!('X-CSRFToken' in config.headers))
+        return { ...config, headers: { ...config.headers, ...BaseHeaders } }
+
+    return config
+}
+
+type TGET = (url: string, config?: AxiosRequestConfig) => Promise<Response>
 const GET: TGET = async (url, config) => {
     try {
         const { data } = await axios.get(U(url), config)
         return { ok: true, data }
     } catch (error) {
-        return { ok: false, error: HandleError(error) }
+        return HandleError(error)
     }
 }
 
-export { GET }
+type TPOST = (
+    url: string,
+    data?: any,
+    config?: AxiosRequestConfig
+) => Promise<Response>
+const POST: TPOST = async (url, post_data, config) => {
+    config = GetCSRFToken(config)
+
+    try {
+        const { data } = await axios.post(U(url), post_data, config)
+        return { ok: true, data }
+    } catch (error) {
+        return HandleError(error)
+    }
+}
+
+export { GET, POST }
