0c17447a6a40bd2d7ad0b887429349817ae284aa
007 <dr007cc@gmail.com>
2022-06-14 07:58:46 +0430

    adding Login Route

--- App/App.tsx
+++ App/App.tsx
@@ -2,16 +2,16 @@ import React, { FC } from 'react'
 
 import { Routes, Route } from 'react-router-dom'
 
-import Dashboard from './Dashboard'
+import Dashboard from 'Dashboard'
+import Login from 'Login'
 
 import './style/base.scss'
 
 const App: FC = () => {
     return (
-        // /admin/*
         <Routes>
             <Route path='/' element={<Dashboard />} />
-            {/* <Route path='/login/' element={<Login />} /> */}
+            <Route path='/login/' element={<Login />} />
             {/* <Route path='/AppX/ModelX/IdX/' element={<ModelX />} /> */}
         </Routes>
     )
