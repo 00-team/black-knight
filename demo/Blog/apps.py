44b0e5daa0e29175e9decd117c4be5d405085d35
007 <dr007cc@gmail.com>
2022-06-15 10:06:22 +0430

    adding a label for demo apps

--- demo/Blog/apps.py
+++ demo/Blog/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class BlogConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Blog'
+    label = 'blogs'
+    verbose_name = 'Blogs'
