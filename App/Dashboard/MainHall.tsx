f00d40023f679b55863bad3e2be5dbb2b8fe5527
007 <dr007cc@gmail.com>
2022-06-22 12:57:44 +0430

    code improvement!

--- App/Dashboard/MainHall.tsx
+++ App/Dashboard/MainHall.tsx
@@ -70,29 +70,18 @@ const MainHall: FC = () => {
 export default MainHall
 
 const SectionInActive = () => {
+    let dn = -0.5
     return (
         <div className='mainhall no-section title'>
-            <span style={{ animationDelay: '0' }}>P</span>
-            <span style={{ animationDelay: '0.5s' }}>l</span>
-            <span style={{ animationDelay: '1s' }}>e</span>
-            <span style={{ animationDelay: '1.5s' }}>a</span>
-            <span style={{ animationDelay: '2s' }}>s</span>
-            <span style={{ animationDelay: '2.5s' }}>e</span>
-            <span className='space'></span>
-            <span style={{ animationDelay: '3s' }}>S</span>
-            <span style={{ animationDelay: '3.5s' }}>e</span>
-            <span style={{ animationDelay: '4s' }}>l</span>
-            <span style={{ animationDelay: '4.5s' }}>e</span>
-            <span style={{ animationDelay: '5s' }}>c</span>
-            <span style={{ animationDelay: '5.5s' }}>t</span>
-            <span className='space'></span>
-            <span style={{ animationDelay: '6s' }}>A</span>
-            <span className='space'></span>
-            <span style={{ animationDelay: '6.5s' }}>M</span>
-            <span style={{ animationDelay: '7s' }}>o</span>
-            <span style={{ animationDelay: '7.5s' }}>d</span>
-            <span style={{ animationDelay: '8s' }}>e</span>
-            <span style={{ animationDelay: '8.5s' }}>l</span>
+            {'Please Select a Model'.split('').map((w, idx) => {
+                if (w === ' ') return <span key={idx} className='space' />
+                dn += 0.5
+                return (
+                    <span key={idx} style={{ animationDelay: `${dn}s` }}>
+                        {w}
+                    </span>
+                )
+            })}
         </div>
     )
 }
