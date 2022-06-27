f3892b05745e84b70350054a96dcd32872e8af90
007 <dr007cc@gmail.com>
2022-06-27 22:34:07 +0430

    updating user admin

--- black_knight/admin/models.py
+++ black_knight/admin/models.py
@@ -7,4 +7,11 @@ class GroupAdmin(ModelAdmin):
 
 
 class UserAdmin(ModelAdmin):
-    pass
+    list_display = 'username', 'email', 'first_name', 'last_name', 'is_staff'
+    list_filter = 'is_staff', 'is_superuser', 'is_active', 'groups'
+    search_fields = 'username', 'first_name', 'last_name', 'email'
+    ordering = 'username',
+    filter_horizontal = (
+        'groups',
+        'user_permissions',
+    )
