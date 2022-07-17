974438388a3716d2af034f944b619186290e1e75
sadra <sadrataghavi1383@gmail.com>
2022-07-17 11:39:54 +0430

    add(login):start login form

--- App/Login/LoginForm.tsx
+++ App/Login/LoginForm.tsx
@@ -3,7 +3,12 @@ import React from 'react'
 import './style/loginform.scss'
 
 const LoginForm = () => {
-    return <div>LoginForm</div>
+    return (
+        <div className='login_form-container'>
+            <div className='login_wrapper'></div>
+            <div className='forgot_password-wrapper'></div>
+        </div>
+    )
 }
 
 export default LoginForm
