694c53b39daf38a8ed0de220b31830d778e43386
sadra <sadrataghavi1383@gmail.com>
2022-07-17 13:36:18 +0430

    add(login):update for login form inps

--- App/Login/LoginForm.tsx
+++ App/Login/LoginForm.tsx
@@ -1,14 +1,48 @@
 import React from 'react'
 
+import { BsPersonFill } from '@react-icons/all-files/bs/BsPersonFill'
+import { FaLock } from '@react-icons/all-files/fa/FaLock'
+
 import './style/loginform.scss'
 
 const LoginForm = () => {
     return (
         <div className='login_form-container'>
-            <div className='login_wrapper'></div>
+            <LoginFormWrapper />
             <div className='forgot_password-wrapper'></div>
         </div>
     )
 }
 
 export default LoginForm
+
+const LoginFormWrapper = () => {
+    return (
+        <div className='login_wrapper'>
+            <h1 className='login_title title'>Login</h1>
+            <div className='form'>
+                <div tabIndex={1} className='input-container'>
+                    <label htmlFor='Username'>
+                        <div className='icon'>
+                            <BsPersonFill size={18} />
+                        </div>
+                        <div className='holder'>Username</div>
+                    </label>
+                    <input type={'text'} id='Username' />
+                </div>
+                <div className='input-container'>
+                    <label htmlFor='Password'>
+                        <div className='icon'>
+                            <FaLock size={18} />
+                        </div>
+                        <div className='holder'>Password</div>
+                    </label>
+                    <input type={'password'} id='Password' />
+                </div>
+            </div>
+            <div className='button-container'>
+                <button>send</button>
+            </div>
+        </div>
+    )
+}
