91d2e6e2a04094bb36c341e66da39152dae571bb
sadra <sadrataghavi1383@gmail.com>
2022-07-20 14:28:53 +0430

    feat(brace): paginator function update

--- App/Dashboard/BraceList/Paginator.tsx
+++ App/Dashboard/BraceList/Paginator.tsx
@@ -1,11 +1,35 @@
 import React, { useState } from 'react'
 
 import { BsQuestion } from '@react-icons/all-files/bs/BsQuestion'
+import { IoMdSend } from '@react-icons/all-files/io/IoMdSend'
 
 import './style/paginator.scss'
 
 const Paginator = () => {
+    // debug
+    const TotalPages = 50
+
+    const [SendPage, setSendPage] = useState({
+        page: 1,
+        status: false,
+    })
     const [IsActive, setIsActive] = useState(false)
+
+    const PageNumber = (value: string) => {
+        if (parseInt(value) > 0 && parseInt(value) !== SendPage.page) {
+            return setSendPage({
+                page: parseInt(value),
+                status: true,
+            })
+        } else if (value.length === 0 || parseInt(value) === 0) {
+            return setSendPage({
+                ...SendPage,
+                status: false,
+            })
+        }
+        console.log('idk')
+        return
+    }
     return (
         <div className='paginator-container'>
             <ul className='paginator-items '>
@@ -15,7 +39,12 @@ const Paginator = () => {
                             key={index}
                             className='paginator-item title_smaller'
                         >
-                            <button className='paginator-link paginator-link-number'>
+                            <button
+                                onClick={() =>
+                                    setSendPage({ ...SendPage, page: index })
+                                }
+                                className='paginator-link paginator-link-number'
+                            >
                                 {index + 1}
                             </button>
                         </li>
@@ -27,8 +56,13 @@ const Paginator = () => {
                     </span>
                 </li>
                 <li className='paginator-item title_smaller'>
-                    <button className='paginator-link paginator-link-number'>
-                        50
+                    <button
+                        onClick={() =>
+                            setSendPage({ ...SendPage, page: TotalPages })
+                        }
+                        className='paginator-link paginator-link-number'
+                    >
+                        {TotalPages}
                     </button>
                 </li>
             </ul>
@@ -37,9 +71,20 @@ const Paginator = () => {
                     type={'number'}
                     placeholder='Page Number...'
                     autoFocus={IsActive}
+                    onChange={e => {
+                        PageNumber(e.target.value)
+                    }}
                 />
-                <button onClick={() => setIsActive(!IsActive)}>
-                    <BsQuestion size={30} fill={'black'} />
+                <button
+                    className={`${SendPage.status ? 'active' : ''}`}
+                    onClick={() => setIsActive(!IsActive)}
+                >
+                    <div className='before'>
+                        <BsQuestion size={30} fill={'black'} />
+                    </div>
+                    <div className='after'>
+                        <IoMdSend size={28} fill={'black'} />
+                    </div>
                 </button>
             </div>
         </div>
