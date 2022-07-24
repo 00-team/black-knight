381a9d20090d8d4602540ecbc15e33ffc3341b9b
sadra <sadrataghavi1383@gmail.com>
2022-07-24 23:08:17 +0430

    refactor(braceForm): add buttons

--- App/Dashboard/BraceForm/index.tsx
+++ App/Dashboard/BraceForm/index.tsx
@@ -4,10 +4,13 @@ import { FaNewspaper } from '@react-icons/all-files/fa/FaNewspaper'
 
 import './style/braceform.scss'
 
+// const confetti = require('canvas-confetti')
+
 // debug
 const modelName = 'Blog'
 
 const BraceForm: FC = () => {
+    const HandleClick = () => {}
     return (
         <div className='brace_form-container'>
             <div className='form-title title'>
@@ -20,6 +23,18 @@ const BraceForm: FC = () => {
                         <FaNewspaper size={30} />
                     </div>
                 </span>
+            </div>
+            <div className='form-data'></div>
+            <div className='form-footer'>
+                <button
+                    className='main'
+                    id='save-btn'
+                    onClick={() => HandleClick()}
+                >
+                    Save
+                </button>
+                <button>Save and continue editing</button>
+                <button>Save and add another</button>
             </div>
         </div>
     )
