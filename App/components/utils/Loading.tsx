2839bdc460c5008ee7c55a51a37073cf3af89dcf
sadra <sadrataghavi1383@gmail.com>
2022-07-24 16:16:09 +0430

    feat(loading): loading text add

--- App/components/utils/Loading.tsx
+++ App/components/utils/Loading.tsx
@@ -5,13 +5,16 @@ import './style/loading.scss'
 const Loading = () => {
     return (
         <div className='loading-container'>
-            <div className='ball'></div>
-            <div className='ball'></div>
-            <div className='ball'></div>
-            <div className='ball'></div>
-            <div className='ball'></div>
-            <div className='ball'></div>
-            <div className='ball'></div>
+            <div className='loading-text title_small'>JUST A SEC...</div>
+            <div className='balls'>
+                <div className='ball'></div>
+                <div className='ball'></div>
+                <div className='ball'></div>
+                <div className='ball'></div>
+                <div className='ball'></div>
+                <div className='ball'></div>
+                <div className='ball'></div>
+            </div>
         </div>
     )
 }
