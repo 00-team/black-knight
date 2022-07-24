548e849041be2df691db3f3cb796e474be8b49ad
sadra <sadrataghavi1383@gmail.com>
2022-07-24 21:04:11 +0430

    refactor(brace-form): some class re-configed

--- App/Dashboard/BraceForm/index.tsx
+++ App/Dashboard/BraceForm/index.tsx
@@ -1,14 +1,25 @@
 import React, { FC } from 'react'
 
+import { FaNewspaper } from '@react-icons/all-files/fa/FaNewspaper'
+
 import './style/braceform.scss'
+
+// debug
+const modelName = 'Blog'
 
 const BraceForm: FC = () => {
     return (
         <div className='brace_form-container'>
-            <div className='form-title title_small'>
-                <div className='icon'></div>
-                <div className='holder'></div>
-                <div className='icon'></div>
+            <div className='form-title title'>
+                <span>
+                    <div className='icon'>
+                        <FaNewspaper size={30} />
+                    </div>
+                    <div className='holder'>Add {modelName}</div>
+                    <div className='icon'>
+                        <FaNewspaper size={30} />
+                    </div>
+                </span>
             </div>
         </div>
     )
