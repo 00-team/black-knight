e740f1561ce5f028a3571ab5cd5c77ee7277d842
sadra <sadrataghavi1383@gmail.com>
2022-07-24 16:08:53 +0430

    feat(loading): update for jsx

--- App/components/utils/Loading.tsx
+++ App/components/utils/Loading.tsx
@@ -1,7 +1,19 @@
 import React from 'react'
 
+import './style/loading.scss'
+
 const Loading = () => {
-    return <div>Loading</div>
+    return (
+        <div className='loading-container'>
+            <div className='ball'></div>
+            <div className='ball'></div>
+            <div className='ball'></div>
+            <div className='ball'></div>
+            <div className='ball'></div>
+            <div className='ball'></div>
+            <div className='ball'></div>
+        </div>
+    )
 }
 
 export default Loading
