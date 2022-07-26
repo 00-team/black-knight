a7c86d91a9693338f081fd4456f086137795dc33
sadra <sadrataghavi1383@gmail.com>
2022-07-26 13:30:05 +0430

    feat(form): init iibc states

--- App/Dashboard/BraceForm/index.tsx
+++ App/Dashboard/BraceForm/index.tsx
@@ -1,4 +1,6 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect, useRef, useState } from 'react'
+
+import { C } from '@00-team/utils'
 
 import { FaNewspaper } from '@react-icons/all-files/fa/FaNewspaper'
 
@@ -10,10 +12,34 @@ import './style/braceform.scss'
 const modelName = 'Blog'
 
 const BraceForm: FC = () => {
+    const BtnsContainer = useRef<HTMLDivElement>(null)
+
+    // is intersecting btns container
+    const [iibc, setiibc] = useState(false)
+    useEffect(() => {
+        if (BtnsContainer.current && !iibc) {
+            var observer = new IntersectionObserver(
+                ([entry]) => {
+                    if (entry && entry.isIntersecting) {
+                        setiibc(true)
+                        observer.disconnect()
+                    }
+                },
+                {
+                    rootMargin: '-75px',
+                }
+            )
+
+            observer.observe(BtnsContainer.current)
+        }
+        return () => {
+            if (observer) observer.disconnect()
+        }
+    }, [BtnsContainer])
     const HandleClick = () => {}
     return (
         <div className='brace_form-container'>
-            <div className='form-title title'>
+            <div className={'form-title title' + C(iibc, 'active')}>
                 <span>
                     <div className='icon'>
                         <FaNewspaper size={30} />
@@ -25,7 +51,7 @@ const BraceForm: FC = () => {
                 </span>
             </div>
             <div className='form-data'></div>
-            <div className='form-footer'>
+            <div className='form-footer' ref={BtnsContainer}>
                 <button
                     className='main'
                     id='save-btn'
