69aaa01de4f8c821527ef0ff2bab50eacedfb335
sadra <sadrataghavi1383@gmail.com>
2022-07-27 16:32:01 +0430

    feat(form): add new npm package for new features

--- App/Dashboard/BraceForm/index.tsx
+++ App/Dashboard/BraceForm/index.tsx
@@ -4,15 +4,54 @@ import { C } from '@00-team/utils'
 
 import { FaNewspaper } from '@react-icons/all-files/fa/FaNewspaper'
 
-import './style/braceform.scss'
+import confetti from 'canvas-confetti'
 
-// const confetti = require('canvas-confetti')
+import './style/braceform.scss'
 
 // debug
 const modelName = 'Blog'
 
 const BraceForm: FC = () => {
     const BtnsContainer = useRef<HTMLDivElement>(null)
+
+    var duration = 7 * 1000
+    var animationEnd = Date.now() + duration
+    var defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 }
+
+    function randomInRange(min: number, max: number) {
+        return Math.random() * (max - min) + min
+    }
+
+    const HandleClick = () => {
+        var interval: any = setInterval(function () {
+            var timeLeft = animationEnd - Date.now()
+
+            if (timeLeft <= 0) {
+                return clearInterval(interval)
+            }
+
+            var particleCount = 50 * (timeLeft / duration)
+            // since particles fall down, start a bit higher than random
+            confetti(
+                Object.assign({}, defaults, {
+                    particleCount,
+                    origin: {
+                        x: randomInRange(0.1, 0.3),
+                        y: Math.random() - 0.2,
+                    },
+                })
+            )
+            confetti(
+                Object.assign({}, defaults, {
+                    particleCount,
+                    origin: {
+                        x: randomInRange(0.7, 0.9),
+                        y: Math.random() - 0.2,
+                    },
+                })
+            )
+        }, 250)
+    }
 
     // is intersecting btns container
     const [iibc, setiibc] = useState(false)
@@ -36,7 +75,6 @@ const BraceForm: FC = () => {
             if (observer) observer.disconnect()
         }
     }, [BtnsContainer])
-    const HandleClick = () => {}
     return (
         <div className='brace_form-container'>
             <div className='form-title title'>
