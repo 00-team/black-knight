1ace858ddc23daf31e8d5aacba09536e5791d015
007 <dr007cc@gmail.com>
2022-08-01 10:45:55 +0430

    feat: add Boolean Field

---
+++ App/components/utils/fields/number.tsx
@@ -0,0 +1,34 @@
+import React, { FC } from 'react'
+
+import { ChoicesField, FieldProps } from '.'
+
+import { BooleanFieldModel } from 'state'
+
+type TBoolean = FC<FieldProps<BooleanFieldModel>>
+const BooleanField: TBoolean = ({ field, change, ...attr }) => {
+    const _ = (b: boolean) => (b ? '1' : '0')
+
+    if (field.choices) {
+        return (
+            <ChoicesField<typeof field.choices[0]>
+                {...attr}
+                onChange={e => change(e.currentTarget.value)}
+                defaultValue={_(field.value || field.initial)}
+                choices={field.choices}
+                get_label={c => c[1]}
+                get_value={c => _(c[0])}
+            />
+        )
+    }
+
+    return (
+        <input
+            {...attr}
+            type='checkbox'
+            defaultChecked={field.value || field.initial}
+            onChange={e => change(_(e.target.checked))}
+        />
+    )
+}
+
+export { BooleanField }
