6a63a0f0c191d9d9e72e7043b8826c954a2bfc4e
007 <dr007cc@gmail.com>
2022-08-01 10:46:10 +0430

    feat: add Text Field

--- App/components/utils/fields/text.tsx
+++ App/components/utils/fields/text.tsx
@@ -1,34 +1,14 @@
-import React, { FC, HTMLAttributes, ReactNode } from 'react'
+import React, { FC } from 'react'
 
-import { FieldProps } from '.'
+import { ChoicesField, FieldProps } from '.'
 
-import { CharFieldModel } from 'state'
-
-interface SelectProps<choice> extends HTMLAttributes<HTMLSelectElement> {
-    choices: choice[]
-    get_label: (c: choice, i: number) => ReactNode
-    get_value: (c: choice, i: number) => string | number
-}
-
-function Select<choice>(props: SelectProps<choice>) {
-    const { choices, get_label, get_value, ...attr } = props
-
-    return (
-        <select {...attr}>
-            {choices.map((c, i) => (
-                <option key={i} value={get_value(c, i)}>
-                    {get_label(c, i)}
-                </option>
-            ))}
-        </select>
-    )
-}
+import { CharFieldModel, TextFieldModel } from 'state'
 
 type TChar = FC<FieldProps<CharFieldModel>>
 const CharField: TChar = ({ field, change, ...attr }) => {
     if (field.choices) {
         return (
-            <Select<typeof field.choices[0]>
+            <ChoicesField<typeof field.choices[0]>
                 {...attr}
                 onChange={e => change(e.currentTarget.value)}
                 defaultValue={field.value || field.initial}
@@ -50,4 +30,30 @@ const CharField: TChar = ({ field, change, ...attr }) => {
     )
 }
 
-export { CharField }
+type TText = FC<FieldProps<TextFieldModel>>
+const TextField: TText = ({ field, change, ...attr }) => {
+    if (field.choices) {
+        return (
+            <ChoicesField<typeof field.choices[0]>
+                {...attr}
+                onChange={e => change(e.currentTarget.value)}
+                defaultValue={field.value || field.initial}
+                choices={field.choices}
+                get_label={c => c[1]}
+                get_value={c => c[0]}
+            />
+        )
+    }
+
+    return (
+        <textarea
+            {...attr}
+            defaultValue={field.value || field.initial}
+            onChange={e => change(e.target.value)}
+            rows={10}
+            cols={50}
+        />
+    )
+}
+
+export { CharField, TextField }
