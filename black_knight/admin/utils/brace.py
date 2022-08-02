49fd3ee865add0bb33e6b007a8e88caf167cc092
007 <dr007cc@gmail.com>
2022-08-02 21:01:24 +0430

    fix: dump json values for response

--- black_knight/admin/utils/brace.py
+++ black_knight/admin/utils/brace.py
@@ -1,5 +1,6 @@
 import datetime
 import decimal
+import json
 
 from black_knight.fields.related import ForeignKey
 from django.db import models
@@ -27,11 +28,17 @@ def display_value(field, value):
             return value
             # return dict(field.flatchoices).get(value)
 
-        if isinstance(field, models.ImageField):
+        elif isinstance(field, models.ImageField):
             return 'image', value.url if value else None
 
-        if isinstance(field, models.ForeignKey):
+        elif isinstance(field, models.ForeignKey):
             return 'foreign_key', value.pk, str(value)
+
+        elif isinstance(field, models.JSONField) and value:
+            try:
+                return json.dumps(value, ensure_ascii=False, cls=field.encoder)
+            except TypeError:
+                return str(value)
 
     if value is None:
         return None
@@ -81,3 +88,58 @@ def update_field(name, instance, data, change):
     else:
         value = field.clean(value, instance)
     field.save_form_data(instance, value)
+
+
+'''
+
+def display_for_field(value, field, empty_value_display):
+    from django.contrib.admin.templatetags.admin_list import _boolean_icon
+
+    if getattr(field, "flatchoices", None):
+        return dict(field.flatchoices).get(value, empty_value_display)
+    # BooleanField needs special-case null-handling, so it comes before the
+    # general null test.
+    elif isinstance(field, models.BooleanField):
+        return _boolean_icon(value)
+    elif value is None:
+        return empty_value_display
+    elif isinstance(field, models.DateTimeField):
+        return formats.localize(timezone.template_localtime(value))
+    elif isinstance(field, (models.DateField, models.TimeField)):
+        return formats.localize(value)
+    elif isinstance(field, models.DecimalField):
+        return formats.number_format(value, field.decimal_places)
+    elif isinstance(field, (models.IntegerField, models.FloatField)):
+        return formats.number_format(value)
+    elif isinstance(field, models.FileField) and value:
+        return format_html('<a href="{}">{}</a>', value.url, value)
+    elif isinstance(field, models.JSONField) and value:
+        try:
+            return json.dumps(value, ensure_ascii=False, cls=field.encoder)
+        except TypeError:
+            return display_for_value(value, empty_value_display)
+    else:
+        return display_for_value(value, empty_value_display)
+
+
+def display_for_value(value, empty_value_display, boolean=False):
+    from django.contrib.admin.templatetags.admin_list import _boolean_icon
+
+    if boolean:
+        return _boolean_icon(value)
+    elif value is None:
+        return empty_value_display
+    elif isinstance(value, bool):
+        return str(value)
+    elif isinstance(value, datetime.datetime):
+        return formats.localize(timezone.template_localtime(value))
+    elif isinstance(value, (datetime.date, datetime.time)):
+        return formats.localize(value)
+    elif isinstance(value, (int, decimal.Decimal, float)):
+        return formats.number_format(value)
+    elif isinstance(value, (list, tuple)):
+        return ", ".join(str(v) for v in value)
+    else:
+        return str(value)
+
+'''
