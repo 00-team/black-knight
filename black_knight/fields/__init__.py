3a088d0ad9351c4df410c4b8144f9721bf167c9a
007 <dr007cc@gmail.com>
2022-08-02 18:38:43 +0430

    fix(backend): import only the required fields

--- black_knight/fields/__init__.py
+++ black_knight/fields/__init__.py
@@ -1,9 +1,14 @@
 from black_knight.fields.base import BaseField
-from black_knight.fields.datetime import *
-from black_knight.fields.files import *
-from black_knight.fields.number import *
-from black_knight.fields.related import *
-from black_knight.fields.text import *
+from black_knight.fields.datetime import DateField, DateTimeField
+from black_knight.fields.files import ImageField
+from black_knight.fields.number import BigIntegerField, BooleanField
+from black_knight.fields.number import DecimalField, FloatField, IntegerField
+from black_knight.fields.number import PositiveBigIntegerField
+from black_knight.fields.number import PositiveIntegerField
+from black_knight.fields.number import PositiveSmallIntegerField
+from black_knight.fields.number import SmallIntegerField
+from black_knight.fields.related import ForeignKey
+from black_knight.fields.text import CharField, TextField
 
 
 __all__ = [
@@ -15,6 +20,12 @@ __all__ = [
     'ImageField',
 
     'BooleanField',
+    'BigIntegerField',
+    'FloatField',
+    'IntegerField',
+    'PositiveSmallIntegerField',
+    'DecimalField',
+    'SmallIntegerField',
     'PositiveIntegerField',
     'PositiveBigIntegerField',
 
