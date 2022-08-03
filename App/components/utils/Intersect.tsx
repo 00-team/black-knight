3ebd250d7e6787173fae1485351b2d1ccda6b777
007 <dr007cc@gmail.com>
2022-08-03 15:48:08 +0430

    feat: add Intersect util

---
+++ App/components/utils/Intersect.tsx
@@ -0,0 +1,49 @@
+import React, {
+    FC,
+    HTMLAttributes,
+    ReactNode,
+    useEffect,
+    useRef,
+    useState,
+} from 'react'
+
+import { C } from '@00-team/utils'
+
+type attrs = Omit<HTMLAttributes<HTMLDivElement>, 'children'>
+interface IntersectProps extends attrs {
+    children: (intersected: boolean) => ReactNode
+    options?: IntersectionObserverInit
+}
+
+const Intersect: FC<IntersectProps> = ({ children, options, ...attrs }) => {
+    const container = useRef<HTMLDivElement>(null)
+    const [Intersected, setIntersected] = useState(false)
+
+    useEffect(() => {
+        if (container.current && !Intersected) {
+            var observer = new IntersectionObserver(([entry]) => {
+                if (entry && entry.isIntersecting) {
+                    setIntersected(true)
+                    observer.disconnect()
+                }
+            }, options)
+
+            observer.observe(container.current)
+        }
+        return () => {
+            if (observer) observer.disconnect()
+        }
+    }, [container])
+
+    return (
+        <div
+            ref={container}
+            {...attrs}
+            className={'intersect ' + C(Intersected) + attrs.className || ''}
+        >
+            {children(Intersected)}
+        </div>
+    )
+}
+
+export { Intersect }
