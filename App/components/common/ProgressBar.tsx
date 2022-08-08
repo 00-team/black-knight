c28fdbe51859891e10314a7e8dd58287ccf3ae3e
sadra <sadrataghavi1383@gmail.com>
2022-08-08 21:54:14 +0430

    feat(progress-bar)

---
+++ App/components/common/ProgressBar.tsx
@@ -0,0 +1,21 @@
+import React, { FC } from 'react'
+
+import './style/progressbar.scss'
+
+interface ProgressBarProps {
+    progress: number
+}
+
+const ProgressBar: FC<ProgressBarProps> = ({ progress }) => {
+    return (
+        <div className='progress_bar-container'>
+            <div
+                className='progress_bar'
+                style={{ width: progress + '%' }}
+            ></div>
+            <span>%{progress}</span>
+        </div>
+    )
+}
+
+export default ProgressBar
