752682647a6dceb4fa84d1fe1fca186f115bfd49
007 <dr007cc@gmail.com>
2022-08-17 17:58:07 +0430

    fix: import fonts

--- App/App.tsx
+++ App/App.tsx
@@ -7,6 +7,7 @@ import { Route, Routes } from 'react-router-dom'
 import { BouncyText } from './components'
 
 import './style/base.scss'
+import './style/fonts/imports.scss'
 
 const Dashboard = loadable(() => import('Dashboard'))
 const BraceForm = loadable(() => import('Dashboard/BraceForm'))
